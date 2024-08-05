c4e6f63fc8c4f56423e5556d66f19af8068d1a53
SushrutReddy <146928322+SushrutReddy@users.noreply.github.com>
2024-08-05 16:04:55 +0000

    prototype

---
+++ 01_basics/prototype.js
@@ -0,0 +1,27 @@
+let temp = 'Hello';
+let a = 10
+let b = true
+let c = '12'
+let final =new String ('2+2')
+// console.log(temp.__proto__);
+// console.log(temp.prototype);
+// console.log(String.prototype);
+// console.log(String.__proto__);
+// console.log(String.prototype.charAt.call(temp,2));
+
+
+// console.log(temp.repeat(10));
+console.log(eval(a));
+console.log(eval(b));
+console.log(eval(c));
+console.log(eval(a+b));
+console.log(eval(final.valueOf()+a));
+
+let num = new Number(2);
+num = num.valueOf()
+console.log(eval(num));
+let num2 = Number.NaN
+console.log(num2);
+console.log(Number.isFinite(num));
+
+
