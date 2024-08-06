81445ae28bb4a4df0202c01a146dc89d8601f3c0
SushrutReddy <146928322+SushrutReddy@users.noreply.github.com>
2024-08-06 02:00:31 +0000

    functions

---
+++ 01_basics/functions.js
@@ -0,0 +1,19 @@
+// function one(){
+// let name = "Sushrut";
+//     console.log(this);
+// }
+// one();
+
+const two = function(){
+    let name = "Sushrut";
+    console.log(this.name);
+}
+
+two();
+
+// const three = () => {
+//     let age = "Sushrut";
+//     console.log(this.age);
+// }
+
+// three();
